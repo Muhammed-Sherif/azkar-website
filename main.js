d3ee8714a6d22c560cdf78fd09cd7e542aef7fd2
Mohamed <143053310+Muhammed-Sherif@users.noreply.github.com>
2023-09-25 05:59:53 +0300

    main.js

--- main.js
+++ main.js
@@ -32,12 +32,14 @@ async function createContent() {
   // get rondom zekr
 
   if (Math.floor(Math.random()*2)===0) {
-    rondom_zekr.innerHTML= azkar_sabah_data[Math.floor(Math.random()*(azkar_sabah_data.length))].mainText
-    zekr_info.innerHTML = azkar_sabah_data[Math.floor(Math.random()*(azkar_sabah_data.length))].infoText
+    let rondom_number = Math.floor(Math.random()*(azkar_sabah_data.length));
+    rondom_zekr.innerHTML= azkar_sabah_data[rondom_number].mainText
+    zekr_info.innerHTML = azkar_sabah_data[rondom_number].infoText
   }
   else {
-    rondom_zekr.innerHTML= azkar_masaa_data[Math.floor(Math.random()*(azkar_masaa_data.length))].mainText
-    zekr_info.innerHTML = azkar_masaa_data[Math.floor(Math.random()*(azkar_masaa_data.length))].infoText
+    let rondom_number = Math.floor(Math.random()*(azkar_masaa_data.length));
+    rondom_zekr.innerHTML= azkar_masaa_data[rondom_number].mainText
+    zekr_info.innerHTML = azkar_masaa_data[rondom_number].infoText
   }
   // all_azkar.forEach((azkar,index)=>{
     function create_zekr_page(azkar_name,azkar_title_name,azkar_data) {
